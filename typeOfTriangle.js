d0cb8e7e2cea909bc61ec08655857ec24d728b02
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-18 22:00:28 +0000

    finished triangle kata

--- typeOfTriangle.js
+++ typeOfTriangle.js
@@ -5,8 +5,11 @@ var typeOfTriangle = function (sideA, sideB, sideC) {
     sideA + sideC <= sideB
     ? "Not a valid triangle"
     : { 1: "Scalene", 2: "Isosceles", 3: "Equilateral" }[
-        [sideA, sideB, sideC].filter((x) => x === sideA ).length
+        Math.max(
+          [sideA, sideB, sideC].filter((x) => x === sideA).length,
+          [sideA, sideB, sideC].filter((x) => x === sideB).length
+        )
       ];
 };
 
-console.log(typeOfTriangle(7,5,5));
+console.log(typeOfTriangle(7, 5, 5));
