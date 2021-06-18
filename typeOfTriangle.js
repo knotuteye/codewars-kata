c160495302c623963e1a58cd49d1abd92edec8bd
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-18 21:40:22 +0000

    modded typeOfTriangle

--- typeOfTriangle.js
+++ typeOfTriangle.js
@@ -1,7 +1,12 @@
 var typeOfTriangle = function (sideA, sideB, sideC) {
-  return [sideA, sideB, sideC].map((x) => parseInt(x)).includes(NaN)
+  return [sideA, sideB, sideC].some((x) => typeof x !== "number" || x < 1) ||
+    sideA + sideB <= sideC ||
+    sideB + sideC <= sideA ||
+    sideA + sideC <= sideB
     ? "Not a valid triangle"
     : { 1: "Scalene", 2: "Isosceles", 3: "Equilateral" }[
-        [sideA, sideB, sideC].filter((x) => x === sideA).length
+        [sideA, sideB, sideC].filter((x) => x === sideA ).length
       ];
 };
+
+console.log(typeOfTriangle(7,5,5));
